f654c0b3c1774a71fcca5b3c6f820d2d04cb514f
kaviyasasi <110805524+kaviyasasi@users.noreply.github.com>
2023-08-17 10:35:09 +0530

    Add files via upload

---
+++ EXP 8 software personal management system/Central_Management_System_1.java
@@ -0,0 +1,117 @@
+
+
+/**
+ * Class Central_Management_System_1
+ */
+public class Central_Management_System_1 {
+
+  //
+  // Fields
+  //
+
+  private void employee_name;
+  private void emp_number;
+  private void details;
+  
+  //
+  // Constructors
+  //
+  public Central_Management_System_1 () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of employee_name
+   * @param newVar the new value of employee_name
+   */
+  private void setEmployee_name (void newVar) {
+    employee_name = newVar;
+  }
+
+  /**
+   * Get the value of employee_name
+   * @return the value of employee_name
+   */
+  private void getEmployee_name () {
+    return employee_name;
+  }
+
+  /**
+   * Set the value of emp_number
+   * @param newVar the new value of emp_number
+   */
+  private void setEmp_number (void newVar) {
+    emp_number = newVar;
+  }
+
+  /**
+   * Get the value of emp_number
+   * @return the value of emp_number
+   */
+  private void getEmp_number () {
+    return emp_number;
+  }
+
+  /**
+   * Set the value of details
+   * @param newVar the new value of details
+   */
+  private void setDetails (void newVar) {
+    details = newVar;
+  }
+
+  /**
+   * Get the value of details
+   * @return the value of details
+   */
+  private void getDetails () {
+    return details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void leave_taken_()
+  {
+  }
+
+
+  /**
+   */
+  public void tax()
+  {
+  }
+
+
+  /**
+   */
+  public void loan()
+  {
+  }
+
+
+  /**
+   */
+  public void salary()
+  {
+  }
+
+
+  /**
+   */
+  public void central_management_system()
+  {
+  }
+
+
+}
---
+++ EXP 8 software personal management system/university.java
@@ -0,0 +1,69 @@
+
+
+/**
+ * Class university
+ */
+public class university {
+
+  //
+  // Fields
+  //
+
+  private void store_details;
+  
+  //
+  // Constructors
+  //
+  public university () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of store_details
+   * @param newVar the new value of store_details
+   */
+  private void setStore_details (void newVar) {
+    store_details = newVar;
+  }
+
+  /**
+   * Get the value of store_details
+   * @return the value of store_details
+   */
+  private void getStore_details () {
+    return store_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void get_access()
+  {
+  }
+
+
+  /**
+   */
+  public void add_and_modify_details()
+  {
+  }
+
+
+  /**
+   */
+  public void view_details()
+  {
+  }
+
+
+}
