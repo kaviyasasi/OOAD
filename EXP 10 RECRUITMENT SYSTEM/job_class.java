a570685ab78e708a50075d7745f8e2017127c0f1
kaviyasasi <110805524+kaviyasasi@users.noreply.github.com>
2023-08-17 14:20:44 +0530

    Add files via upload

---
+++ EXP 10 RECRUITMENT SYSTEM/job_class.java
@@ -0,0 +1,89 @@
+
+
+/**
+ * Class job_class
+ */
+public class job_class {
+
+  //
+  // Fields
+  //
+
+  private void Add_jobs;
+  private void search_jobs;
+  private void view_job;
+  
+  //
+  // Constructors
+  //
+  public job_class () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of Add_jobs
+   * @param newVar the new value of Add_jobs
+   */
+  private void setAdd_jobs (void newVar) {
+    Add_jobs = newVar;
+  }
+
+  /**
+   * Get the value of Add_jobs
+   * @return the value of Add_jobs
+   */
+  private void getAdd_jobs () {
+    return Add_jobs;
+  }
+
+  /**
+   * Set the value of search_jobs
+   * @param newVar the new value of search_jobs
+   */
+  private void setSearch_jobs (void newVar) {
+    search_jobs = newVar;
+  }
+
+  /**
+   * Get the value of search_jobs
+   * @return the value of search_jobs
+   */
+  private void getSearch_jobs () {
+    return search_jobs;
+  }
+
+  /**
+   * Set the value of view_job
+   * @param newVar the new value of view_job
+   */
+  private void setView_job (void newVar) {
+    view_job = newVar;
+  }
+
+  /**
+   * Get the value of view_job
+   * @return the value of view_job
+   */
+  private void getView_job () {
+    return view_job;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void update()
+  {
+  }
+
+
+}
