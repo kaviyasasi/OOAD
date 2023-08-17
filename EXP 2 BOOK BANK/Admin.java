13faf3385c79aa6ed344b304842b6ebd4bfe013d
kaviyasasi <110805524+kaviyasasi@users.noreply.github.com>
2023-08-17 14:13:08 +0530

    Add files via upload

---
+++ EXP 2 BOOK BANK/Admin.java
@@ -0,0 +1,100 @@
+
+
+/**
+ * Class Admin
+ */
+public class Admin {
+
+  //
+  // Fields
+  //
+
+  private void student_details;
+  private void booklist;
+  
+  //
+  // Constructors
+  //
+  public Admin () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of student_details
+   * @param newVar the new value of student_details
+   */
+  private void setStudent_details (void newVar) {
+    student_details = newVar;
+  }
+
+  /**
+   * Get the value of student_details
+   * @return the value of student_details
+   */
+  private void getStudent_details () {
+    return student_details;
+  }
+
+  /**
+   * Set the value of booklist
+   * @param newVar the new value of booklist
+   */
+  private void setBooklist (void newVar) {
+    booklist = newVar;
+  }
+
+  /**
+   * Get the value of booklist
+   * @return the value of booklist
+   */
+  private void getBooklist () {
+    return booklist;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void verify_student_id()
+  {
+  }
+
+
+  /**
+   */
+  public void check_book_availability()
+  {
+  }
+
+
+  /**
+   */
+  public void issue_book()
+  {
+  }
+
+
+  /**
+   */
+  public void order_for_new_author()
+  {
+  }
+
+
+  /**
+   */
+  public void maintain_stud_details()
+  {
+  }
+
+
+}
