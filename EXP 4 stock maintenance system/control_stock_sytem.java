6d44e8fdffc3dcd3279dfdeb80db587c803a593a
kaviyasasi <110805524+kaviyasasi@users.noreply.github.com>
2023-08-17 10:24:33 +0530

    Add files via upload

---
+++ EXP 4 stock maintenance system/control_stock_sytem.java
@@ -0,0 +1,62 @@
+
+
+/**
+ * Class control_stock_sytem
+ */
+public class control_stock_sytem {
+
+  //
+  // Fields
+  //
+
+  private void store_stock_details;
+  
+  //
+  // Constructors
+  //
+  public control_stock_sytem () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of store_stock_details
+   * @param newVar the new value of store_stock_details
+   */
+  private void setStore_stock_details (void newVar) {
+    store_stock_details = newVar;
+  }
+
+  /**
+   * Get the value of store_stock_details
+   * @return the value of store_stock_details
+   */
+  private void getStore_stock_details () {
+    return store_stock_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void print_bill()
+  {
+  }
+
+
+  /**
+   */
+  public void delivery_product()
+  {
+  }
+
+
+}
