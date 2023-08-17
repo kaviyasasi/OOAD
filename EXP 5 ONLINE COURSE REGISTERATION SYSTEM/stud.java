45654388442a0d06cd86e5c26d503412535ac8cc
kaviyasasi <110805524+kaviyasasi@users.noreply.github.com>
2023-08-17 14:17:39 +0530

    Add files via upload

---
+++ EXP 5 ONLINE COURSE REGISTERATION SYSTEM/stud.java
@@ -0,0 +1,103 @@
+
+
+/**
+ * Class stud
+ */
+public class stud {
+
+  //
+  // Fields
+  //
+
+  private void name;
+  private void address_;
+  private void marks;
+  
+  //
+  // Constructors
+  //
+  public stud () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of name
+   * @param newVar the new value of name
+   */
+  private void setName (void newVar) {
+    name = newVar;
+  }
+
+  /**
+   * Get the value of name
+   * @return the value of name
+   */
+  private void getName () {
+    return name;
+  }
+
+  /**
+   * Set the value of address_
+   * @param newVar the new value of address_
+   */
+  private void setAddress_ (void newVar) {
+    address_ = newVar;
+  }
+
+  /**
+   * Get the value of address_
+   * @return the value of address_
+   */
+  private void getAddress_ () {
+    return address_;
+  }
+
+  /**
+   * Set the value of marks
+   * @param newVar the new value of marks
+   */
+  private void setMarks (void newVar) {
+    marks = newVar;
+  }
+
+  /**
+   * Get the value of marks
+   * @return the value of marks
+   */
+  private void getMarks () {
+    return marks;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void browse()
+  {
+  }
+
+
+  /**
+   */
+  public void select()
+  {
+  }
+
+
+  /**
+   */
+  public void register()
+  {
+  }
+
+
+}
