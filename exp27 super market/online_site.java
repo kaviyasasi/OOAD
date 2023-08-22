a9f7e95bbf48ced838e2fd8a7d7b698e7f7b737b
kaviyasasi <110805524+kaviyasasi@users.noreply.github.com>
2023-08-22 17:05:14 +0530

    Add files via upload

---
+++ exp27 super market/online_site.java
@@ -0,0 +1,96 @@
+
+
+/**
+ * Class online_site
+ */
+public class online_site {
+
+  //
+  // Fields
+  //
+
+  private void _login;
+  private void id;
+  private void _name;
+  
+  //
+  // Constructors
+  //
+  public online_site () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of _login
+   * @param newVar the new value of _login
+   */
+  private void set_login (void newVar) {
+    _login = newVar;
+  }
+
+  /**
+   * Get the value of _login
+   * @return the value of _login
+   */
+  private void get_login () {
+    return _login;
+  }
+
+  /**
+   * Set the value of id
+   * @param newVar the new value of id
+   */
+  private void setId (void newVar) {
+    id = newVar;
+  }
+
+  /**
+   * Get the value of id
+   * @return the value of id
+   */
+  private void getId () {
+    return id;
+  }
+
+  /**
+   * Set the value of _name
+   * @param newVar the new value of _name
+   */
+  private void set_name (void newVar) {
+    _name = newVar;
+  }
+
+  /**
+   * Get the value of _name
+   * @return the value of _name
+   */
+  private void get_name () {
+    return _name;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void _add()
+  {
+  }
+
+
+  /**
+   */
+  public void _update()
+  {
+  }
+
+
+}
