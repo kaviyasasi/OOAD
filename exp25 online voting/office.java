16e22c50608ba3e0ae0a092596c2a11e7ab7f120
kaviyasasi <110805524+kaviyasasi@users.noreply.github.com>
2023-08-22 17:04:30 +0530

    Add files via upload

---
+++ exp25 online voting/office.java
@@ -0,0 +1,79 @@
+
+
+/**
+ * Class office
+ */
+public class office {
+
+  //
+  // Fields
+  //
+
+  private void _can_symbol;
+  private void _candidate_details;
+  
+  //
+  // Constructors
+  //
+  public office () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of _can_symbol
+   * @param newVar the new value of _can_symbol
+   */
+  private void set_can_symbol (void newVar) {
+    _can_symbol = newVar;
+  }
+
+  /**
+   * Get the value of _can_symbol
+   * @return the value of _can_symbol
+   */
+  private void get_can_symbol () {
+    return _can_symbol;
+  }
+
+  /**
+   * Set the value of _candidate_details
+   * @param newVar the new value of _candidate_details
+   */
+  private void set_candidate_details (void newVar) {
+    _candidate_details = newVar;
+  }
+
+  /**
+   * Get the value of _candidate_details
+   * @return the value of _candidate_details
+   */
+  private void get_candidate_details () {
+    return _candidate_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void update()
+  {
+  }
+
+
+  /**
+   */
+  public void _add()
+  {
+  }
+
+
+}
---
+++ exp25 online voting/online_site.java
@@ -0,0 +1,96 @@
+
+
+/**
+ * Class online_site
+ */
+public class online_site {
+
+  //
+  // Fields
+  //
+
+  private void _login;
+  private void _id;
+  private void details;
+  
+  //
+  // Constructors
+  //
+  public online_site () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of _login
+   * @param newVar the new value of _login
+   */
+  private void set_login (void newVar) {
+    _login = newVar;
+  }
+
+  /**
+   * Get the value of _login
+   * @return the value of _login
+   */
+  private void get_login () {
+    return _login;
+  }
+
+  /**
+   * Set the value of _id
+   * @param newVar the new value of _id
+   */
+  private void set_id (void newVar) {
+    _id = newVar;
+  }
+
+  /**
+   * Get the value of _id
+   * @return the value of _id
+   */
+  private void get_id () {
+    return _id;
+  }
+
+  /**
+   * Set the value of details
+   * @param newVar the new value of details
+   */
+  private void setDetails (void newVar) {
+    details = newVar;
+  }
+
+  /**
+   * Get the value of details
+   * @return the value of details
+   */
+  private void getDetails () {
+    return details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void _update()
+  {
+  }
+
+
+  /**
+   */
+  public void _add()
+  {
+  }
+
+
+}
