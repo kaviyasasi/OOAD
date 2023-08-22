404a0f528f9ea013420f2da92d0bc43d181e5bd5
kaviyasasi <110805524+kaviyasasi@users.noreply.github.com>
2023-08-22 17:06:26 +0530

    Add files via upload

---
+++ exp29 bus tickets/office.java
@@ -0,0 +1,96 @@
+
+
+/**
+ * Class office
+ */
+public class office {
+
+  //
+  // Fields
+  //
+
+  private void seats_count;
+  private void _seats_available;
+  private void _seats_booked;
+  
+  //
+  // Constructors
+  //
+  public office () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of seats_count
+   * @param newVar the new value of seats_count
+   */
+  private void setSeats_count (void newVar) {
+    seats_count = newVar;
+  }
+
+  /**
+   * Get the value of seats_count
+   * @return the value of seats_count
+   */
+  private void getSeats_count () {
+    return seats_count;
+  }
+
+  /**
+   * Set the value of _seats_available
+   * @param newVar the new value of _seats_available
+   */
+  private void set_seats_available (void newVar) {
+    _seats_available = newVar;
+  }
+
+  /**
+   * Get the value of _seats_available
+   * @return the value of _seats_available
+   */
+  private void get_seats_available () {
+    return _seats_available;
+  }
+
+  /**
+   * Set the value of _seats_booked
+   * @param newVar the new value of _seats_booked
+   */
+  private void set_seats_booked (void newVar) {
+    _seats_booked = newVar;
+  }
+
+  /**
+   * Get the value of _seats_booked
+   * @return the value of _seats_booked
+   */
+  private void get_seats_booked () {
+    return _seats_booked;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void _updates()
+  {
+  }
+
+
+  /**
+   */
+  public void _add()
+  {
+  }
+
+
+}
---
+++ exp29 bus tickets/online_site.java
@@ -0,0 +1,96 @@
+
+
+/**
+ * Class online_site
+ */
+public class online_site {
+
+  //
+  // Fields
+  //
+
+  private void _login;
+  private void _id;
+  private void _details;
+  
+  //
+  // Constructors
+  //
+  public online_site () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of _login
+   * @param newVar the new value of _login
+   */
+  private void set_login (void newVar) {
+    _login = newVar;
+  }
+
+  /**
+   * Get the value of _login
+   * @return the value of _login
+   */
+  private void get_login () {
+    return _login;
+  }
+
+  /**
+   * Set the value of _id
+   * @param newVar the new value of _id
+   */
+  private void set_id (void newVar) {
+    _id = newVar;
+  }
+
+  /**
+   * Get the value of _id
+   * @return the value of _id
+   */
+  private void get_id () {
+    return _id;
+  }
+
+  /**
+   * Set the value of _details
+   * @param newVar the new value of _details
+   */
+  private void set_details (void newVar) {
+    _details = newVar;
+  }
+
+  /**
+   * Get the value of _details
+   * @return the value of _details
+   */
+  private void get_details () {
+    return _details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void _update()
+  {
+  }
+
+
+  /**
+   */
+  public void _add()
+  {
+  }
+
+
+}
